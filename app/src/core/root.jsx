1047b4a9e89eb40c52061ccb24810c7c72d46a94
Avi <you@example.com>
2020-05-30 23:29:21 +0300

    Change root to a functional component

--- app/src/core/root.jsx
+++ app/src/core/root.jsx
@@ -9,30 +9,26 @@ import 'react-redux-toastr/lib/css/react-redux-toastr.min.css'
 import styles from "./root.scss";
 import "./root.global.scss";
 
-class Root extends React.Component {
-  render() {
-    const { store, history } = this.props;
-
-    return (
-      <Provider store={store}>
-        <ReduxToastr
-          timeOut={4000}
-          newestOnTop={false}
-          preventDuplicates
-          position="bottom-right"
-          getState={(state) => state.toastr}
-          transitionIn="fadeIn"
-          transitionOut="fadeOut"
-          progressBar
-          closeOnToastrClick />
-        <ConnectedRouter history={history}>
-          <div className={styles.container}>
-            <Routes></Routes>
-          </div>
-        </ConnectedRouter>
-      </Provider>
-    );
-  }
+const Root = ({ store, history }) => {
+  return (
+    <Provider store={store}>
+      <ReduxToastr
+        timeOut={4000}
+        newestOnTop={false}
+        preventDuplicates
+        position="bottom-right"
+        getState={(state) => state.toastr}
+        transitionIn="fadeIn"
+        transitionOut="fadeOut"
+        progressBar
+        closeOnToastrClick />
+      <ConnectedRouter history={history}>
+        <div className={styles.container}>
+          <Routes></Routes>
+        </div>
+      </ConnectedRouter>
+    </Provider>
+  );
 }
 
 export default Root;

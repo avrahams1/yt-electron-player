28b19685e4dd0fab173e88e49909e8a16e04f713
Avi <you@example.com>
2020-06-02 22:23:05 +0300

    derp

--- app/src/redux/utils/reduxStoreUtils.js
+++ app/src/redux/utils/reduxStoreUtils.js
@@ -5,38 +5,41 @@ export const REMEMBER_PLAYLIST = "rememberPlaylistsSelection";
 export const PLAYLIST_ITEMS_PROP = playlistId => `playlist-${playlistId}`;
 
 export function writeValue(propName, value) {
-  return sendStoreRequestAndRegisterForResponse({
-    requestType: writeConfigRequest,
-    responseType: writeConfigResponse,
-    additionalParams: [propName, value]
-  }).then(() => value);
+  return sendStoreRequestAndRegisterForResponse(
+    writeConfigRequest,
+    writeConfigResponse,
+    true,
+    propName,
+    value
+  ).then(() => value);
 }
 
 export function readValue(propName, clearBindings = true) {
-  return sendStoreRequestAndRegisterForResponse({
-    requestType: readConfigRequest,
-    responseType: readConfigResponse,
+  return sendStoreRequestAndRegisterForResponse(
+    readConfigRequest,
+    readConfigResponse,
     clearBindings,
-    additionalParams: [propName]
-  }).then(args => {
+    propName
+  ).then(args => {
     const { key, success, value } = args;
 
     if (key === propName && success && value) {
-      return args.value;
+      return value;
     }
 
     return Promise.reject();
   })
 }
 
-function sendStoreRequestAndRegisterForResponse({ requestType, responseType, clearBindings = true, additionalParams = [] }) {
+function sendStoreRequestAndRegisterForResponse(requestType, responseType, clearBindings, ...additionalParams) {
+  const { store } = window.api;
+
   if (clearBindings) {
-    window.api.store.clearRendererBindings();
+    store.clearRendererBindings();
   }
 
   return new Promise(resolve => {
-    window.api.store.onReceive(responseType, resolve);
-
-    window.api.store.send(requestType, ...additionalParams);
+    store.onReceive(responseType, resolve);
+    store.send(requestType, ...additionalParams);
   });
 }
